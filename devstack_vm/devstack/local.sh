e28d51087448b8eb935914a84717b4c2907bdd2f
Remus Buzatu <r.buzatu90@gmail.com>
2016-03-02 14:14:35 +0200

    Set tempest conf to min_compute_nodes 2

--- devstack_vm/devstack/local.sh
+++ devstack_vm/devstack/local.sh
@@ -34,7 +34,11 @@ TEMPEST_CONFIG=/opt/stack/tempest/etc/tempest.conf
 
 iniset $TEMPEST_CONFIG compute volume_device_name "sdb"
 iniset $TEMPEST_CONFIG compute-feature-enabled rdp_console true
+
+iniset $TEMPEST_CONFIG compute min_compute_nodes 2
 iniset $TEMPEST_CONFIG compute-feature-enabled block_migrate_cinder_iscsi False
+iniset $TEMPEST_CONFIG compute-feature-enabled block_migration_for_live_migration True
+iniset $TEMPEST_CONFIG compute-feature-enabled live_migration True
 
 iniset $TEMPEST_CONFIG scenario img_dir "/home/ubuntu/devstack/files/images/"
 iniset $TEMPEST_CONFIG scenario img_file "cirros-0.3.3-x86_64.vhdx"
