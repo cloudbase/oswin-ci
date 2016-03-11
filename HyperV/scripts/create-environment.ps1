0b43b341784cc60fdc31d4b5ab8cd266dfad38b5
Claudiu Belu <bclau@users.noreply.github.com>
2016-03-11 20:58:35 +0200

    Removes unnecessary cherry-pick
    Removes networking-hyperv patch - cannot be applied anymore.

--- HyperV/scripts/create-environment.ps1
+++ HyperV/scripts/create-environment.ps1
@@ -217,10 +217,6 @@ ExecRetry {
 
 ExecRetry {
     pushd C:\OpenStack\build\openstack\networking-hyperv
-    # Checks if port features are being added
-    Write-Host "Doing fetch... refs/changes/28/265728/7"
-    git fetch https://review.openstack.org/openstack/nova refs/changes/28/265728/7
-    cherry_pick FETCH_HEAD
     & pip install C:\OpenStack\build\openstack\networking-hyperv
     if ($LastExitCode) { Throw "Failed to install networking-hyperv from repo" }
 }
