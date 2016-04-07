a567268e76ea4091620ff0118fb48a20f5af983a
Remus Buzatu <r.buzatu90@gmail.com>
2016-04-07 15:41:33 +0300

    Removed hard-coded paths and fine tuned the firewall

--- jobs/run_initialize.sh
+++ jobs/run_initialize.sh
@@ -181,9 +181,34 @@ run_ssh_cmd_with_retry ubuntu@$FLOATING_IP $DEVSTACK_SSH_KEY 'mkdir -p /openstac
 echo `date -u +%H:%M:%S` "Started to unzip Fedora image.."
 run_ssh_cmd_with_retry ubuntu@$FLOATING_IP $DEVSTACK_SSH_KEY "gzip --decompress --force /home/ubuntu/devstack/files/images/Fedora-x86_64-20-20140618-sda.vhdx.gz"
 
+
+#Get IP addresses of the two Hyper-V hosts
+
+set +e
+IFS='' read -r -d '' PSCODE <<'_EOF'
+$NetIPAddr = Get-NetIPAddress | Where-Object {$_.InterfaceAlias -like "*br100*" -and $_.AddressFamily -like "IPv4"}
+$IPAddr = $NetIPAddr.IPAddress
+Write-Host $IPAddr
+_EOF
+HYPERV_GET_DATA_IP=`echo "$PSCODE" | iconv -f ascii -t utf16le | base64 -w0`
+hyperv01_ip=`run_wsman_cmd $hyperv01 $WIN_USER $WIN_PASS "powershell -ExecutionPolicy RemoteSigned -EncodedCommand $HYPERV_GET_DATA_IP" 2>&1 | grep -E -o '10\.0\.[0-9]{1,2}\.[0-9]{1,3}'`
+hyperv02_ip=`run_wsman_cmd $hyperv02 $WIN_USER $WIN_PASS "powershell -ExecutionPolicy RemoteSigned -EncodedCommand $HYPERV_GET_DATA_IP" 2>&1 | grep -E -o '10\.0\.[0-9]{1,2}\.[0-9]{1,3}'`
+set -e
+
+echo `date -u +%H:%M:%S` "Data IP of $hyperv01 is $hyperv01_ip"
+echo `date -u +%H:%M:%S` "Data IP of $hyperv02 is $hyperv02_ip"
+if [[ ! $hyperv01_ip =~ ^10\.0\.[0-9]{1,2}\.[0-9]{1,3} ]]; then
+    echo "Did not receive a good IP for Hyper-V host $hyperv01 : $hyperv01_ip"
+    exit 1
+fi
+if [[ ! $hyperv02_ip =~ ^10\.0\.[0-9]{1,2}\.[0-9]{1,3} ]]; then
+    echo "Did not receive a good IP for Hyper-V host $hyperv02 : $hyperv02_ip"
+    exit 1
+fi
+
 # Building devstack as a threaded job
 echo `date -u +%H:%M:%S` "Started to build devstack as a threaded job"
-nohup /usr/local/src/oswin-ci/jobs/build_devstack.sh > /home/jenkins-slave/logs/devstack-build-log-$ZUUL_UUID 2>&1 &
+nohup /usr/local/src/oswin-ci/jobs/build_devstack.sh $hyperv01_ip $hyperv02_ip > /home/jenkins-slave/logs/devstack-build-log-$ZUUL_UUID 2>&1 &
 pid_devstack=$!
 
 # Building and joining HyperV nodes
