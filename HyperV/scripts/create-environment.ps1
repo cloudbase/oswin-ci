4f5f2795d6e9fc5921ec7d7a71d026fa9af72721
Claudiu Belu <bclau@users.noreply.github.com>
2016-04-18 16:05:56 +0300

    Force Babel==2.2.0
    Services don't start because of conflicting versions of Babel.

--- HyperV/scripts/create-environment.ps1
+++ HyperV/scripts/create-environment.ps1
@@ -195,6 +195,7 @@ Add-Content "$env:APPDATA\pip\pip.ini" $pip_conf_content
 & pip install -U --pre pymi
 & pip install cffi
 & pip install numpy
+& pip install Babel==2.2.0
 popd
 
 $hasPipConf = Test-Path "$env:APPDATA\pip"
